3c39b7d62c19c8c4c827801d2ade3f6ab6581f95
MasterRueangyotTreemat <ruengyod1@gmail.com>
2019-05-25 17:05:05 +0700

    Created RestaurantRandom!

---
+++ app/src/main/java/com/noomnim/myapplication/RestaurantRandom.java
@@ -0,0 +1,40 @@
+package com.noomnim.myapplication;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RestaurantRandom extends Activity {
+
+    TextView textView;
+    Button button;
+    String list[] = {
+            "MK Restaurant",
+            "KFC",
+            "Chesters Grill",
+            "Bar-B-Q Plaza",
+            "The Pizza Company"
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_random);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(getRestaurantName());
+            }
+        });
+    }
+
+    public String getRestaurantName() {
+        return list[(int) Math.round(Math.random()*list.length - 1)];
+    }
+}
